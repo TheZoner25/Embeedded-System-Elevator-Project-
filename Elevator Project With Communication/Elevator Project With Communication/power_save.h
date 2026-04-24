98d6e7b307549aa209cac32cb64e528bdc49f6bd
nabihataahsin <nabiha.taahsin@gmail.com>
2026-04-24 12:44:01 +0300

    Update power_save.h

--- Elevator Project With Communication/Elevator Project With Communication/power_save.h
+++ Elevator Project With Communication/Elevator Project With Communication/power_save.h
@@ -1,8 +1,9 @@
-#ifndef POWER_SAVE_H
-#define POWER_SAVE_H
+//Preprocessor directive, Defines a symbol
+#ifndef POWER_SAVE_H 
+#define POWER_SAVE_H 
 
 #include <stdint.h>
-
+//These are prototypes
 void power_save_init(void);
 void enter_light_sleep(void);
 
